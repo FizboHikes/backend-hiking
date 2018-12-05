16294b36edca0361c4e75844b5f0fd0bb86227aa
learn <learn@learn08.localdomain>
2018-12-05 09:58:54 -0800

    implemented the Profile table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_005730) do
+ActiveRecord::Schema.define(version: 2018_12_05_174616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "username"
+    t.text "about"
+    t.text "interests"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +39,5 @@ ActiveRecord::Schema.define(version: 2018_12_04_005730) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
