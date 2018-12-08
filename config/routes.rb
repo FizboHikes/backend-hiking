8447f3f7bae1cc4aa913af952cc40bd72228a600
Eric Uyemura <ea.uyemura@gmail.com>
2018-12-07 20:57:23 -0800

    fixed the routes in order to obtain user hikes, it was a naming error in the routes v controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :hikes
   resources :profiles
   devise_for :users, defaults: { format: :json }
-  get 'users/:id/hikes' => 'hikes#user_hikes'
+  get 'users/:user_id/hikes' => 'hikes#user_hikes'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
