e714ce82d8749b52104a2b8f74ca44be1b818930
Eric Uyemura <ea.uyemura@gmail.com>
2018-12-11 09:19:44 -0800

    added a default value for images, that doesn't work

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,3 +1,9 @@
 class Hike < ApplicationRecord
   belongs_to :user
+  after_create :set_defaults
+
+  def set_defaults
+    self.image ||= "https://i.pinimg.com/originals/77/85/91/7785910e63b1662e8abe313c8ef9d160.jpg"
+  end
+
 end
