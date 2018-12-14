cb76101a28b08b2c877664e4bfecdb66c5422243
Mi Sun Choi <choi.msc033@gmail.com>
2018-12-14 13:37:26 -0800

    added a default interview

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,9 +1,11 @@
 class Hike < ApplicationRecord
   belongs_to :user
-  after_create :set_defaults
+  before_create :set_defaults
 
   def set_defaults
-    self.image ||= "https://i.pinimg.com/originals/77/85/91/7785910e63b1662e8abe313c8ef9d160.jpg"
+    if(self.image == '')
+      self.image = "https://i.pinimg.com/originals/77/85/91/7785910e63b1662e8abe313c8ef9d160.jpg"
+    end
   end
 
 end
